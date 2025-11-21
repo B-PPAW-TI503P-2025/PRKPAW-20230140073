6eff8b7b5631229c009732a84f29ba2074cceaae
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-21 09:11:38 +0700

    ucp1

--- pawpraktikum1_react_20230140073/src/App.js
+++ pawpraktikum1_react_20230140073/src/App.js
@@ -1,30 +1,49 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
 import DashboardPage from "./components/DashboardPage";
+import PresensiPage from "./components/PresensiPage";
+import ReportsPage from "./components/ReportsPage";
+import Navbar from "./components/Navbar";
+
+function AppContent() {
+  const location = useLocation();
+
+  // Halaman yang tidak menampilkan navbar
+  const hideNavbarOn = ["/login", "/register"];
+
+  const shouldHideNavbar = hideNavbarOn.includes(location.pathname);
+
+  return (
+    <>
+      {/* Tampilkan Navbar jika bukan login/register */}
+      {!shouldHideNavbar && <Navbar />}
+
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/presensi" element={<PresensiPage />} />
+        <Route path="/reports" element={<ReportsPage />} />
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div>
-        {" "}
-        {/* Navigasi ini bisa dihapus jika tidak diperlukan */}{" "}
-        <nav className="p-4 bg-gray-100">
-          <Link to="/login" className="mr-4">
-            {" "}
-            Login{" "}
-          </Link>{" "}
-          <Link to="/register"> Register </Link>{" "}
-        </nav>{" "}
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />{" "}
-          <Route path="/register" element={<RegisterPage />} />{" "}
-          <Route path="/dashboard" element={<DashboardPage />} />{" "}
-          <Route path="/" element={<LoginPage />} />{" "}
-        </Routes>{" "}
-      </div>{" "}
+      <AppContent />
     </Router>
   );
 }
+
 export default App;
