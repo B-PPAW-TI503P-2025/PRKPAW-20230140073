05469a46ea71d92e885da5564ab5a14c91e3bf6d
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-21 09:46:38 +0700

    ucp1

--- pawpraktikum1_node_20230140073/models/presensi.js
+++ pawpraktikum1_node_20230140073/models/presensi.js
@@ -4,7 +4,6 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Presensi extends Model {
     static associate(models) {
-      // Relasi: Presensi milik satu User
       Presensi.belongsTo(models.User, {
         foreignKey: "userId",
         as: "user",
